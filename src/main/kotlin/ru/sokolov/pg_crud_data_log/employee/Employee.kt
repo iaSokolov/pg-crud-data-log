030601edf1b89d69fc3f09e4353f20395434a2a2
Sokolov Ilya <iasokolov@gmail.com>
2024-01-01 21:59:00 +0300

    Добавил плагин allopen для JPA

--- src/main/kotlin/ru/sokolov/pg_crud_data_log/employee/Employee.kt
+++ src/main/kotlin/ru/sokolov/pg_crud_data_log/employee/Employee.kt
@@ -1,17 +1,21 @@
 package ru.sokolov.pg_crud_data_log.employee
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
 import jakarta.persistence.Table
 import java.util.*
 
 @Entity
 @Table(name = "employee")
-data class Employee(
+class Employee(
     @Id
+    @Column(name = "id")
     val id: UUID,
 
+    @Column(name = "op_id")
     val opId: UUID,
 
+    @Column(name = "name")
     val name: String
 )
--- src/main/kotlin/ru/sokolov/pg_crud_data_log/operation/Operation.kt
+++ src/main/kotlin/ru/sokolov/pg_crud_data_log/operation/Operation.kt
@@ -1,5 +1,6 @@
 package ru.sokolov.pg_crud_data_log.operation
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.Table
@@ -7,13 +8,17 @@ import java.util.*
 
 @Entity
 @Table(name = "operation")
-data class Operation(
+class Operation(
     @Id
+    @Column(name = "id")
     val id: UUID,
 
+    @Column(name = "system")
     val system: String,
 
+    @Column(name = "module")
     val module: String,
 
+    @Column(name = "chanel")
     val chanel: String
 )
